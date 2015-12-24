a46ea6e5011295830e328e4864eefc203ecd81d8
William-Hai <return_zero0@163.com>
2015-12-24 15:10:12 +0800

    更新 poj-2388

---
+++ src/org/poj/p2388/Main.java
@@ -0,0 +1,38 @@
+package org.poj.p2388;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        a = sort(a);
+
+        System.out.println(a[n / 2]);
+        scanner.close();
+    }
+    
+    private static int[] sort(int[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    array[i] = array[i] + array[j];
+                    array[j] = array[i] - array[j];
+                    array[i] = array[i] - array[j];
+                }
+            }
+        }
+
+        return array;
+    }
+}
