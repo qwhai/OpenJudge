5d55658cbb16d642de698cef3b1bdbd763e71135
William-Hai <return_zero0@163.com>
2016-03-10 09:52:31 +0800

    3265:aaa

---
+++ src/org/poj/_p3265/Main.java
@@ -0,0 +1,17 @@
+package org.poj._p3265;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            reader.readLine();
+            System.out.println("aa");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
